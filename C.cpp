27114dc3d0b02e4c1d96d3dc6d109e31093ab0ab
Igor <igor.santos5@estudante.ufla.br>
2019-04-17 18:23:51 -0300

    Criacao da Classe C

---
+++ C.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class C{
+	string 	C1;
+	int C2;
+	
+	void MC1(){
+		cout << "MC1" << endl;
+	};
+	
+	void MC2(){
+		cout << "MC2" << endl;
+	};
+};
