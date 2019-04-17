c5f09a738b64d691d8a37bd69b0e299a0dc9cffb
S0AR3S <49733251+S0AR3S@users.noreply.github.com>
2019-04-17 18:00:17 -0300

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class A{
+	
+	int A1;
+	float A2;
+	
+	int getA1(){
+		return A1;
+	};
+	
+	float getA2(){
+		return A2;
+	};
+	
+	void MA1(){
+		cout << "MA1" << endl;
+	};
+	
+	void MA2(){
+		cout << "MA2" << endl;
+	};
+
+};
+
+
---
+++ B.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class B{
+	
+	int B1;
+	float B2;
+	
+	int getB1(){
+		return B1;
+	};
+	
+	float getB2(){
+		return B2;
+	};
+	
+	void MB1(){
+		cout << "MB1" << endl;
+	};
+	
+	void MB2(){
+		cout << "MB2" << endl;
+	};
+};
